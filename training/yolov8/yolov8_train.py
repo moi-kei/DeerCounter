e72f3979c6e4c65acca7b659aa22bc5bb031fc6b
moi-kei <michaelmackenzie98@gmail.com>
2024-02-20 16:47:56 +0000

    test run of yolov8, 10 epochs, 100m flights

--- training/yolov8/yolov8_train.py
+++ training/yolov8/yolov8_train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8l.pt")
 
-results = model.train(data="datasets/data.yaml", epochs=100)
+results = model.train(data="datasets/data.yaml", epochs=10)
