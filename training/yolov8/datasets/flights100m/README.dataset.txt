8a2b5dabb6bcbd835441691e18318768d3e787aa
moi-kei <michaelmackenzie98@gmail.com>
2024-03-13 16:53:00 +0000

    removed training files/datasets as they have been moved to the cloud

--- training/yolov8/datasets/flights100m/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# 100m flights > 2024-02-19 8:41pm
-https://universe.roboflow.com/project-yk2dj/100m-flights
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- training/yolov8/yolov8_train.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-#change to last.pt when training again
-model = YOLO("yolov8l.pt")
-
-results = model.train(data="datasets/data.yaml", epochs=10)
