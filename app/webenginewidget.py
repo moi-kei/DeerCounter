ba713b2ac1f05e8a6badd04ba7ac83e22d1f49c5
moi-kei <michaelmackenzie98@gmail.com>
2024-03-28 17:08:06 +0000

    images with detected deer is marked on the map in a different colour

--- app/webenginewidget.py
+++ app/webenginewidget.py
@@ -29,21 +29,21 @@ class WebEngineWidget(QWidget):
             max_lon = max(max_lon, lon)
             min_lat = min(min_lat, lat)
             min_lon = min(min_lon, lon)
-
-            # Determine marker color
-            marker_color = 'red' if item[0] == image_name else 'blue'
+            detected_objects = len(item[3])
 
             popup = folium.Popup(item[0])
             if item[0]==image_name:
-                folium.Marker([lat, lon], popup=popup, icon=folium.Icon(color=marker_color)).add_to(m)
+                folium.Marker([lat, lon], popup=popup, icon=folium.Icon(color='red')).add_to(m)
+            elif detected_objects > 0:
+                folium.CircleMarker(location=[lat, lon], radius=7, popup=popup, color='orange', fill=True, fill_opacity=1).add_to(m)
             else:
-                folium.CircleMarker(location=[lat, lon], radius=5, popup=popup, color=marker_color, fill=True, fill_opacity=1).add_to(m)
+                folium.CircleMarker(location=[lat, lon], radius=5, popup=popup, color='blue', fill=True, fill_opacity=1).add_to(m)
 
             # Add coordinates for the polyline
             polyline_coords.append([lat, lon])
 
         # Add polyline to the map
-        folium.PolyLine(polyline_coords, color='blue').add_to(m)
+        folium.PolyLine(polyline_coords, color='blue', dash_array='5').add_to(m)
 
         m.fit_bounds([[min_lat, min_lon], [max_lat, max_lon]])
         map_html = m.get_root().render()
